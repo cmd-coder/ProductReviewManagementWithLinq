663f6b916a8793fa313846b1560eae2530de393b
CMD <gautamkailashsingh@gmail.com>
2020-11-08 13:40:22 +0530

    Add project files.

---
+++ ProductReviewManagementWithLinq/Management.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementWithLinq
+{
+    class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " UserID: " + list.UserID + " Rating: " + list.Rating + " Review: " + list.Review + " isLike: " + list.isLike);
+            }
+        }
+
+        public void SelectRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9)
+                               && productReviews.Rating > 3
+                               select productReviews;
+
+            foreach(var list in recordedData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " UserID: " + list.UserID + " Rating: " + list.Rating + " Review: " + list.Review + " isLike: " + list.isLike);
+            }
+        }
+
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() }) ;
+            foreach(var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID + "--------" + list.Count);
+            }
+        }
+    }
+}
