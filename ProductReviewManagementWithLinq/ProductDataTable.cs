d0517a84e774bac3f05cf9059a891d4a5c18a3df
CMD <gautamkailashsingh@gmail.com>
2020-11-08 22:06:16 +0530

    [Gautam] Retreive all records from the list who’s Userid=10 and order by rating.

--- ProductReviewManagementWithLinq/ProductDataTable.cs
+++ ProductReviewManagementWithLinq/ProductDataTable.cs
@@ -12,22 +12,22 @@ namespace ProductReviewManagementWithLinq
         {
             DataTable productTable = new DataTable();
             productTable.Columns.Add("ProductID");
-            productTable.Columns.Add("UserID");
+            productTable.Columns.Add("UserID", typeof(int));
             productTable.Columns.Add("Rating", typeof(int));
             productTable.Columns.Add("Review");
             productTable.Columns.Add("isLike", typeof(bool));
 
-            productTable.Rows.Add("1", "1", 2, "Good", true);
-            productTable.Rows.Add("2", "1", 4, "Good", true);
-            productTable.Rows.Add("3", "1", 5, "Good", true);
-            productTable.Rows.Add("4", "1", 6, "Good", false);
-            productTable.Rows.Add("5", "1", 2, "Nice", true);
-            productTable.Rows.Add("6", "1", 1, "Bad", true);
-            productTable.Rows.Add("8", "1", 1, "Good", false);
-            productTable.Rows.Add("8", "1", 9, "Nice", true);
-            productTable.Rows.Add("2", "1", 10, "Good", true);
-            productTable.Rows.Add("10", "1", 8, "Nice", true);
-            productTable.Rows.Add("11", "1", 3, "Nice", true);
+            productTable.Rows.Add("1", 10, 2, "Good", true);
+            productTable.Rows.Add("2", 10, 4, "Good", true);
+            productTable.Rows.Add("3", 10, 5, "Good", true);
+            productTable.Rows.Add("4", 10, 6, "Good", false);
+            productTable.Rows.Add("5", 10, 2, "Nice", true);
+            productTable.Rows.Add("6", 1, 1, "Bad", true);
+            productTable.Rows.Add("8", 1, 1, "Good", false);
+            productTable.Rows.Add("8", 1, 9, "Nice", true);
+            productTable.Rows.Add("2", 1, 10, "Good", true);
+            productTable.Rows.Add("10", 1, 8, "Nice", true);
+            productTable.Rows.Add("11", 1, 3, "Nice", true);
 
             Console.WriteLine("---------------------");
             Console.WriteLine("Use Case 8: Create Data Table And Store Some Default Values");
@@ -48,6 +48,11 @@ namespace ProductReviewManagementWithLinq
             Console.WriteLine("Use Case 11: All The Records With \"Nice\" Reviews");
             Console.WriteLine("---------------------");
             AllNiceReviews(productTable);
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Use Case 12: Order All The Records By Their Rating With UserID=10");
+            Console.WriteLine("---------------------");
+            OrderAllRecordsWithUserID10(productTable);
 
         }
 
@@ -81,6 +86,15 @@ namespace ProductReviewManagementWithLinq
         public static void AllNiceReviews(DataTable productTable)
         {
             var recordedData = from products in productTable.AsEnumerable() where products.Field<string>("Review") == "Nice" select products;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID: " + list[0] + " UserID: " + list[1] + " Rating: " + list[2] + " Review: " + list[3] + " isLike: " + list[4]);
+            }
+        }
+
+        public static void OrderAllRecordsWithUserID10(DataTable productTable)
+        {
+            var recordedData = from products in productTable.AsEnumerable() where products.Field<int>("UserID") == 10 orderby products.Field<int>("Rating") select products;
             foreach (var list in recordedData)
             {
                 Console.WriteLine("ProductID: " + list[0] + " UserID: " + list[1] + " Rating: " + list[2] + " Review: " + list[3] + " isLike: " + list[4]);
